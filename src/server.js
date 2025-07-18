58132cea7269ea1d39f540a75c81f252ee31005c
Sumit Gupta <sumitgupta7955@gmai.com>
2025-07-18 18:28:02 +0530

    refactor server.js to organize imports and initialize user routes

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import dotenv from "dotenv"
+import userRoutes from "./routes/user.routes.js";
 dotenv.config({
     path: './env'
 })
 import connectDB from './db/index.js';
-
+const app = express();
 connectDB();
+
+app.use('/api/users', userRoutes);
